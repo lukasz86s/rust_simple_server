29ce8a60c41ea381c42f2a2a7d623f00f3f360a7
Łukasz Surmiak <fetts@o2.pl>
2023-05-12 19:56:44 +0200

    first draft

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+
+use server::Server;
+use http::Method;
+use http::Request;
+
+mod server;
+mod http;
+
+fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+
+    // let str = String::from("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
