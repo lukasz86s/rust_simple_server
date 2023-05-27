cba59f769e83a4aea2ded41ec42fbabf37b9b69f
Łukasz Surmiak <fetts@o2.pl>
2023-05-27 19:59:27 +0200

    implement simple run funcito

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 use server::Server;
 use http::Method;
 use http::Request;
+use std::net::TcpListener;
+
 
 mod server;
 mod http;
 
 fn main() {
+    
     let get = Method::GET;
     let delete = Method::DELETE;
     let post = Method::POST;
@@ -14,4 +17,5 @@ fn main() {
     // let str = String::from("127.0.0.1:8080");
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
+    
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::{net::TcpListener, io::Error};
+use std::io::Read;
 pub struct Server{
     addr: String,
 }
@@ -9,10 +11,34 @@ impl Server{
         }
     }
 
-    pub fn run(self){
-        
+    pub fn run<'a>(self) -> (){
         println!("{}", self.addr);
         println!("start servera");
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop{
+            
+            match listener.accept() {
+                Ok((mut stream, _)) =>{
+                    println!(" OK ");
+                    let mut buffer = [0; 1024]; // create array with 1024 zeroes values
+                    match stream.read(& mut buffer){
+                        Ok(_) => {
+                            print!("Recived a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => print!("Failed to read from connection: {}", e),
+                    }
+                    
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+            
+            
+        }
+
+        
+        
+        
 
     }
 
