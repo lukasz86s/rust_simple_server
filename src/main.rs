cfb087481184bcae7cf1f83984b50fc064f14795
Łukasz Surmiak <fetts@o2.pl>
2023-08-10 23:51:24 +0200

    added reading .css files

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 use server::Server;
 use http::Method;
 use http::Request;
-use std::net::TcpListener;
+// use std::net::TcpListener;
+use std::env;
+use std::path::Path;
 use website_handler::WebsiteHandler;
 
 mod server;
@@ -11,13 +13,10 @@ mod http;
 mod website_handler;
 
 fn main() {
-    
-    let get = Method::GET;
-    let delete = Method::DELETE;
-    let post = Method::POST;
+    let default_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("public");
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path.to_string_lossy().into_owned() );
 
-    // let str = String::from("127.0.0.1:8080");
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
     
 }
